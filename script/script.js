f5bc9dc3062ae1ddb6402a7177f3db055cfa7070
Alana Lipkin <alana.lipkin@gmail.com>
2021-12-22 14:02:00 -0500

    change character to pickle and broke game

--- script/script.js
+++ script/script.js
@@ -1,27 +1,37 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
+// const character = document.getElementById("character");
+const image = document.getElementById("image");
+const block = document.getElementById("block");
 
+const score = document.getElementById("score");
+const gameOver = document.getElementById("game-over");
+let isDead = false;
 
-
-
+let scoreCounter = 0;
 function jump(){
-    if(character.classList != "animate"){
-    character.classList.add("animate");
+    if(image.classList !== "animate"){
+    image.classList.add("animate");
     }
     setTimeout(function(){
-       character.classList.remove("animate");
+       image.classList.remove("animate");
+           
     },500);
 }
+const upDateScore = setInterval(function (){
+    if (!isDead){scoreCounter = scoreCounter +10; 
+    score.textContent = scoreCounter;}
+},2000);
 
-var checkDead = setInterval(function(){
-    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+const checkDead = setInterval(function(){
+    const imageTop = parseInt(window.getComputedStyle(image).getPropertyValue("top"));
 
-    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
 
-    if(blockLeft<20 && blockLeft>0 &&  characterTop>=130){
+    if(blockLeft<20 && blockLeft>0 && imageTop>=130){
             block.style.animation = "none";
             block.style.display = "none";
-            alert("You lose!");
+            isDead = true;
+            // alert("You lose!");
+            gameOver.textContent = "You lose!";
     }
 
 },10);
